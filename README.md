ade79ac304e674c70e9674c22c6ae686fa157656
arnavpatial21 <rakeshpatial78@gmail.com>
2021-05-27 10:39:48 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-r7kw1n
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-r7kw1n)
---
+++ index.js
@@ -0,0 +1,5 @@
+var runner = require('code-runner');
+runner(code,options,function(){
+  //callback function
+  ...
+});
